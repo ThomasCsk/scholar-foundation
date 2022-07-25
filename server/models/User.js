b16125e08cddd0d2e139472ce8b78b88c3d913ff
Thomas Ciszek <tciszek515@gmail.com>
2022-07-25 14:48:17 -0400

    server-side models created

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    admin: {
+      // 0 if student, 1 if staff/admin
+      type: Boolean,
+      required: true
+    }
+  }
+)
+
+const User = model('User', userSchema);
+
+module.exports = User;
