d94836731a59bb98f5c9f7a7f2c061906190f502
chlucas324 <chlucas324@gmail.com>
2022-07-29 18:59:44 -0400

    update seeds

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,67 +1,177 @@
-const faker = require('faker');
 const db = require('../config/connection');
 const { User, Application } = require('../models');
 
-const seed = async () => {
-    try {
-        await db.sync({ force: true })
+db.once('open', async () => {
+  await User.deleteMany();
 
-        //declare a variable and set it equal to an array
-        let users = []
+  await User.create({
+    username: 'sbooks',
+    email: 'sbooks@gmail.com',
+    password: 'passwordsb',
+    name: 'Susan Books',
+    admin: '1' 
+    });
 
-        //this for loop decides how many datapoints you will create
-        //if you want to change the amount, just change the number in the for loop
-        for (let i = 0; i < 100; i++) {
-            const name = faker.name.fullName()
+await User.create({
+    username: 'csmith',
+    email: 'csmith@gmail.com',
+    password: 'passwordcs',
+    name: 'Caroline Smith',
+    admin: '1' 
+    });
 
-            // the keys in this user object are set equal to the fake information
-            let newUser = {
-                id: faker.datatype.number(),
-                username: faker.internet.userName(),
-                email:faker.internet.email(name).toLowerCase(),
-                password: faker.internet.password(),
-                name: name,
-                admin: faker.random.boolean(),
-            }
+await User.create({
+    username: 'bgray',
+    email: 'bgray@gmail.com',
+    password: 'passwordbg',
+    name: 'Brian Gray',
+    admin: '1' 
+    });
 
-            // For each fake user you create, you're going to push them into the user array you declare above
-            users.push(newUser)
-        }
+await User.create({
+    username: 'cjones',
+    email: 'cjones@gmail.com',
+    password: 'passwordcj',
+    name: 'Charles Johnes',
+    admin: '1' 
+    });
 
-          // For each user in the array, you are going to create a new user instance in the database
-    users.forEach(async (user) => {
-        await User.create(user)
-    })
-} catch(err) {
-    console.log(err)
-}
+await User.create({
+    username: 'tbrown',
+    email: 'tbrown@gmail.com',
+    password: 'passwordtb',
+    name: 'Taylor Brown',
+    admin: '1' 
+    });
 
-    let applications = []
+await User.create({
+    username: 'rbragg',
+    email: 'rbragg@gmail.com',
+    password: 'passwordrb',
+    name: 'Riley Bragg',
+    admin: '0' 
+    });
 
-    for(let i = 0; i < 100; i++ ) {
-        const appName = faker.name.fullName()
+await User.create({
+    username: 'hbeckett',
+    email: 'hbeckett@gmail.com',
+    password: 'passwordhb',
+    name: 'Haley Beckett',
+    admin: '0' 
+    });
 
-        let newApplication = {
-            createdAt: faker.date.recent(),
-            name:  appName,
-            birthday: faker.date.birthdate({ min: 11, max: 18, mode: 'age'}),
-            phoneNumber: faker.phone.phoneNumberFormat(),
-            addressStreet: faker.address.streetAddress(),
-            addressCity: faker.address.city(),
-            state: faker.address.state(),
-            zipCode: faker.address.zipCode(),
-            guardianName: faker.name.fullName(),
-            guardianPhone: faker.phone.phoneNumberFormat(),
-            grade: faker.datatype.number({'min': 6, 'max': 12}),
-            school: faker.company.name(),
-            currentStatus: faker.datatype.number({'min': 0, 'max': 2}),
-        }
+await User.create({
+    username: 'jgreen',
+    email: 'jgreen@gmail.com',
+    password: 'passwordjg',
+    name: 'Jayden Green',
+    admin: '0' 
+    });
 
-        applications.push(newApplication)
-    }
-    applications.forEach(async (application) => {
-        await Application.create(application)
-        
-      })
-     }
-    
+await User.create({ 
+    username: 'rthomas',
+    email: 'rthomas@gmail.com',
+    password: 'passwordrt',
+    name: 'Ryan Thomas',
+    admin: '0' 
+    });
+
+await User.create({
+    username: 'acastle',
+    email: 'acastle@gmail.com',
+    password: 'passwordac',
+    name: 'Aria Castle',
+    admin: '0' 
+    });
+ 
+console.log('users seeded');
+
+await Application.deleteMany();
+
+await Application.create({
+    createdAt: '06/23/2022',
+    name: 'Pamela Rams',
+    birthday: '05/12/2011',
+    phoneNumber: '123-345-9876',
+    addressStreet: '123 Easy Lane',
+    addressCity: 'Baltimore',
+    state: 'Maryland',
+    zipCode: '54392',
+    guardianName: 'Richard Tuner',
+    guardianPhone: '123-434-8769',
+    grade: '6',
+    school: 'Biscuit Run School',
+    currentStatus: '1'
+ });
+
+ await Application.create({
+    createdAt: '07/23/2022',
+    name: 'Jonathan Sims',
+    birthday: '08/21/2010',
+    phoneNumber: '903-234-8392',
+    addressStreet: '24 Floyd Street',
+    addressCity: 'Floyd',
+    state: 'Virginia',
+    zipCode: '12392',
+    guardianName: 'Katherine Sims',
+    guardianPhone: '123-345-6683',
+    grade: '7',
+    school: 'Mountain View School',
+    currentStatus: '0'
+ });
+
+ await Application.create({
+    createdAt: '04/20/2022',
+    name: 'Kate Lopez',
+    birthday: '02/27/2009',
+    phoneNumber: '583-238-2318',
+    addressStreet: '73736 Track Trail',
+    addressCity: 'Wilmington',
+    state: 'North Carolina',
+    zipCode: '43627',
+    guardianName: 'Maria Lopez',
+    guardianPhone: '949-392-9302',
+    grade: '8',
+    school: 'Whistler School',
+    currentStatus: '2'
+ });
+
+ await Application.create({
+    createdAt: '06/23/2022',
+    name: 'Stella Grain',
+    birthday: '01/25/2007',
+    phoneNumber: '213-123-4563',
+    addressStreet: '21931 Reasons Court',
+    addressCity: 'Five Valleys',
+    state: 'New York',
+    zipCode: '12912',
+    guardianName: 'Randall Grain',
+    guardianPhone: '743-123-5984',
+    grade: '10',
+    school: 'Four Seasons School',
+    currentStatus: '1'
+ });
+
+ await Application.create({
+    createdAt: '06/23/2022',
+    name: 'Marie Joyce',
+    birthday: '05/12/2008',
+    phoneNumber: '321-329-2312',
+    addressStreet: '10294 Rainy Days Drive',
+    addressCity: 'Lewes',
+    state: 'Delaware',
+    zipCode: '32028',
+    guardianName: 'James Joyce',
+    guardianPhone: '126-392-5803',
+    grade: '9',
+    school: 'Journey School',
+    currentStatus: '0'
+ });
+     
+
+  
+
+  console.log('applications seeded');
+
+  process.exit();
+});
