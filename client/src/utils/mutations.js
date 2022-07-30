3d69f3d954c7955cf06c7ff9930c99d759126621
chlucas324 <chlucas324@gmail.com>
2022-07-30 18:21:46 -0400

    add staff info

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $name: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      name: $name
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
