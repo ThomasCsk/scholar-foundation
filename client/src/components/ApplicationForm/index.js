0d6460a9d9abfc03d81d061ea23b2713c49484f1
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-08-02 17:05:23 -0400

    beginning of application

--- client/src/components/ApplicationForm/index.js
+++ client/src/components/ApplicationForm/index.js
@@ -3,17 +3,12 @@ import React, { useState } from 'react';
 const ApplicationForm = () => {
     return (
       <div>
-        <p className="application-form">
-        </p>
-        <form className="flex-row justify-center justify-space-between-md align-stretch">
-          <textarea
-            placeholder="Here's a new thought..."
-            className="form-input col-12 col-md-9"
-          ></textarea>
-          <button className="btn" type="submit">
-            Submit
-          </button>
-        </form>
+      <form className="application-form">
+        <div className='container'>
+          <h1>Scholarship Application</h1>
+          <p>Please fill out this form in its entirety. Double check all answers before submitting.</p>
+        </div>
+      </form>
       </div>
     );
   };
