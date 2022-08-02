d848bc3371e31a1343d6fd1ff8a312d564b705a8
chlucas324 <chlucas324@gmail.com>
2022-08-02 19:24:24 -0400

    update mutatations

--- client/src/pages/LogInSignUp.js
+++ client/src/pages/LogInSignUp.js
@@ -1,146 +1,134 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import { Link } from 'react-router-dom';
-// import { LOGIN } from '../utils/mutations';
-// import Auth from '../utils/auth';
-// import { ADD_USER } from '../utils/mutations';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-// function LogInSignUp(props) {
-//   const [formState, setFormState] = useState({ email: '', password: '' });
-//   const [login, { error }] = useMutation(LOGIN);
-//   const [addUser] = useMutation(ADD_USER);
 
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     try {
-//       const mutationResponse = await login({
-//         variables: { email: formState.email, password: formState.password },
-//       });
-//       const token = mutationResponse.data.login.token;
-//       Auth.login(token);
-//     } catch (e) {
-//       console.log(e);
-//     }
-
-//     const mutationResponse = await addUser({
-//       variables: {
-//         email: formState.email,
-//         password: formState.password,
-//         firstName: formState.firstName,
-//         lastName: formState.lastName,
-//       },
-//   };
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormState({
-//       ...formState,
-//       [name]: value,
-//     });
-//   };
-
-//   // const handleFormSubmit = async (event) => {
-//   //   event.preventDefault();
-   
-//   //   });
-//     const token = mutationResponse.data.addUser.token;
-//     Auth.login(token);
-//   };
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setFormState({
-//       ...formState,
-//       [name]: value,
-//     });
-//   };
+function LogInSignUp(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
+  const [addUser] = useMutation(ADD_USER);
   
 
-//   return (
-//     <div>
-//     <div className="container my-1">
-//      <h2>Login</h2>
-//       <form onSubmit={handleFormSubmit}>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="name">Username:</label>
-//           <input
-//             placeholder="username"
-//             name="text"
-//             type="text"
-//             id="username"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="pwd">Password:</label>
-//           <input
-//             placeholder="******"
-//             name="password"
-//             type="password"
-//             id="pwd"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         {error ? (
-//           <div>
-//             <p className="error-text">The provided credentials are incorrect</p>
-//           </div>
-//         ) : null}
-//         <div className="flex-row flex-end">
-//           <button type="submit">Submit</button>
-//         </div>
-//       </form>
-//     </div>
-//     <div className="container my-1">
-//       <h2>Signup</h2>
-//       <form onSubmit={handleFormSubmit}>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="Name">Name:</label>
-//           <input
-//             placeholder="Name"
-//             name="name"
-//             type="name"
-//             id="name"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="name">Username:</label>
-//           <input
-//             placeholder="username"
-//             name="text"
-//             type="text"
-//             id="username"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="email">Email:</label>
-//           <input
-//             placeholder="youremail@test.com"
-//             name="email"
-//             type="email"
-//             id="email"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row space-between my-2">
-//           <label htmlFor="pwd">Password:</label>
-//           <input
-//             placeholder="******"
-//             name="password"
-//             type="password"
-//             id="pwd"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="flex-row flex-end">
-//           <button type="submit">Submit</button>
-//         </div>
-//       </form>
-//     </div>
-//     </div>
-//   );
-// }
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { username: formState.username, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
 
-// export default LogInSignUp
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        username: formState.username,
+        name: formState.name,
+      },
+  });
+
+  const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+    <div className="container my-1">
+     <h2>LOGIN</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="name">Username:</label>
+          <input
+            placeholder="username"
+            name="text"
+            type="text"
+            id="login_username"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="login_pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+    <div className="container my-1">
+      <h2>SIGN UP</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="Name">Name:</label>
+          <input
+            placeholder="Name"
+            name="name"
+            type="name"
+            id="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="name">Username:</label>
+          <input
+            placeholder="username"
+            name="text"
+            type="text"
+            id="signup_username"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="signuppwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+export default LogInSignUp;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $username: String!
+    $name: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username  
+      name: $name
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
